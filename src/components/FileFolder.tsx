6a1bf9cf16e29af28b8d1252a71c8a5baa028eb8
Dinujaya Sandaruwan <info.dinujaya@gmail.com>
2023-05-18 13:57:04 +0530

    Corrected issues in design and made it responsive for mobile

--- src/components/FileFolder.tsx
+++ src/components/FileFolder.tsx
@@ -6,73 +6,73 @@ import { useState } from "react";
 import { humanBytes } from "../utils/Functions";
 
 interface Props {
-  name: string;
-  size: number;
-  isFile: boolean;
+	name: string;
+	size: number;
+	isFile: boolean;
 }
 
 async function sleep(ms: number) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
+	return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
 const FileFolder = ({ name, size, isFile }: Props) => {
-  const [status, setStatus] = useState("");
-  const [iconVisible, setIconVisible] = useState(true);
+	const [status, setStatus] = useState("");
+	const [iconVisible, setIconVisible] = useState(true);
 
-  const handleClick = async (e: any) => {
-    const isFile = e.target.getAttribute("aria-is-file");
-    const fileName = e.target.getAttribute("aria-file-name");
-    if (isFile === "true") {
-      window.open(`/${fileName}`, "_blank");
-    } else {
-      setStatus("Zipping...");
-      setIconVisible(false);
-      for (let i = 0; i < 101; i++) {
-        await sleep(50);
-        setStatus(`Zipping (${i}%)`);
-      }
-    }
-  };
+	const handleClick = async (e: any) => {
+		const isFile = e.target.getAttribute("aria-is-file");
+		const fileName = e.target.getAttribute("aria-file-name");
+		if (isFile === "true") {
+			window.open(`/${fileName}`, "_blank");
+		} else {
+			setStatus("Zipping...");
+			setIconVisible(false);
+			for (let i = 0; i < 101; i++) {
+				await sleep(50);
+				setStatus(`Zipping (${i}%)`);
+			}
+		}
+	};
 
-  return (
-    <>
-      <div className="flex flex-col sm:flex-row items-center justify-between bg-gray-900 text-white border border-none rounded-md m-2">
-        <div className="flex items-center">
-          <div className="h-7 w-7 bg-gradient-to-r from-green-500 to-cyan-600 rounded-lg flex items-center justify-center">
-            {isFile ? (
-              <FiFile className="text-lg text-white" />
-            ) : (
-              <HiFolder className="text-lg text-white" />
-            )}
-          </div>
-          <div className="ml-2">
-            <p className="text-base">
-              {name}{" "}
-              <span className="text-sm text-blue-500">
-                ({humanBytes(size)})
-              </span>
-            </p>
-          </div>
-        </div>
-        <div className="px-1">
-          <button
-            aria-is-file={isFile}
-            aria-file-name={name}
-            className="bg-blue-600 text-white px-2 py-1 rounded-md hover:bg-blue-500"
-            onClick={handleClick}
-          >
-            {isFile
-              ? iconVisible && (
-                  <FaCloudDownloadAlt className="text-2xl text-white" />
-                )
-              : iconVisible && <MdFolderZip className="text-2xl text-white" />}
-            {status}
-          </button>
-        </div>
-      </div>
-      <hr className="border-gray-800" />
-    </>
-  );
+	return (
+		<>
+			<div className="flex flex-row sm:flex-row items-center justify-between bg-gray-900 text-white border border-none rounded-md m-2">
+				<div className="flex items-center">
+					<div className="h-7 w-7 bg-gradient-to-r from-green-500 to-cyan-600 rounded-lg flex items-center justify-center">
+						{isFile ? (
+							<FiFile className="text-lg text-white" />
+						) : (
+							<HiFolder className="text-lg text-white" />
+						)}
+					</div>
+					<div className="ml-2">
+						<p className="text-base">
+							{name}{" "}
+							<span className="text-sm text-blue-500">
+								({humanBytes(size)})
+							</span>
+						</p>
+					</div>
+				</div>
+				<div className="px-1">
+					<button
+						aria-is-file={isFile}
+						aria-file-name={name}
+						className="bg-blue-600 text-white px-2 py-1 rounded-md hover:bg-blue-500"
+						onClick={handleClick}
+					>
+						{isFile
+							? iconVisible && (
+									<FaCloudDownloadAlt className="text-2xl text-white" />
+							  )
+							: iconVisible && <MdFolderZip className="text-2xl text-white" />}
+						{status}
+					</button>
+				</div>
+			</div>
+			<hr className="border-gray-800" />
+		</>
+	);
 };
 
 export default FileFolder;
